f559906b4bdb6471d48acf077637d2025f99dc6b
lucasszulansky <lucas.szulansky@gmail.com>
2023-08-29 11:13:32 -0700

    Initial commit

---
+++ Lesson_1/PEDAC.rb
@@ -0,0 +1,28 @@
+# PROBLEM:
+
+# Given a string, write a method change_me which returns the same
+# string but with all the words in it that are palindromes uppercased.
+
+# change_me("We will meet at noon") == "We will meet at NOON"
+# change_me("No palindromes here") == "No palindromes here"
+# change_me("") == ""
+# change_me("I LOVE my mom and dad equally") == "I LOVE my MOM and DAD equally"
+
+def change_me(string)
+  array = string.split(' ')
+  counter = 0
+
+  array.each do |word|
+    if word == word.reverse
+      word.upcase!
+    end
+  end
+  
+  string = array.join(' ')
+  string
+end
+
+p change_me("We will meet at noon") == "We will meet at NOON"
+p change_me("No palindromes here") == "No palindromes here"
+p change_me("") == ""
+p change_me("I LOVE my mom and dad equally") == "I LOVE my MOM and DAD equally"
